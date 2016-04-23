1cdf70455476792e42a702c6bf509e4c0af248f7
Pradyumn Agrawal <pradyumnag1997@gmail.com>
2016-04-24 01:43:11 +0530

    Changes in indentation and comments added
    Comments are added to give the clear idea about the what is the function of program. And Indentation is added to make the program user-friendly to understand.

--- sample.c
+++ sample.c
@@ -1,10 +1,11 @@
+//This C Program calculates the sum of numbers upto n
 #include<stdio.h>
 int main()
 {
-int n,count,sum=0;
-for(count=1;count<=n;++count)
-{
-sum+=count;
-}
+  int n,count,sum=0;
+  for(count=1;count<=n;++count)
+  {
+    sum+=count;
+  }
 return 0;
 }
